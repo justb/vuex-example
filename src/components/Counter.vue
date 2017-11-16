f40014190dba1ff75078e4e1be5a6c77ec2a0528
lwei <1390996503@qq.com>
2017-11-16 23:25:08 +0800

    class

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -11,27 +11,35 @@
     <div>{{fooCount}}</div>
     <button @click="inc()">Increment async</button>
     <div>{{msg}}</div>
-    <foo number="我是从父组件传过来的" @showbox="toshow"></foo>
+    <foo number="我是从父组件传过来的" @showbox="toshow"  v-bind:class="{ active: isActive, 'text-danger': hasError }"></foo>
     <bar number="我是从父组件传过来的" ></bar>
   </div>
 </template>
 
 <script>
-
 import foo from "./Foo";
 import bar from "./Bar";
 export default {
   data() {
     return {
-      msg: ""
+      msg: "",
+      count: "123",
+      isActive: true,
+      hasError: false
     };
   },
   destroyed() {
     // this.$store.unregisterModule("foo");
   },
   computed: {
-    fooCount() {
-      return this.$store.state.counter.count;
+    fooCount: {
+      get() {
+        // return "123";
+        return this.$store.state.counter.count;
+      },
+      set(v) {
+        console.log(v);
+      }
     }
   },
   beforeCreate(store) {
@@ -47,8 +55,21 @@ export default {
       this.$store.dispatch("counter/inc");
     },
     toshow(msg) {
-        this.msg = msg;
+      this.msg = msg;
+    },
+    fooCount2() {
+      return Date.now();
+    }
+  },
+  watch: {
+    msg(v) {
+      return v;
     }
   }
 };
 </script>
+<style>
+.active{
+  border: 2px solid red !important;
+}
+</style>
