9dd7fed878275fc4b22560dd8d19a3cc7534c393
Lewis <lewis.wei@foxmail.com>
2017-11-20 18:50:37 +0800

    axios

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
-
+import cfg from '../../config/dev.json'
+console.log(cfg)
 export const addToCart = ({ commit }, product) => {
-      setTimeout(() => {
-        commit("add", {
-            id: 1
-          })
-      }, 1000);
+    axios.get(cfg.club.host + "/api/v1/games/" + 506 + "?app_key=" + cfg.club.app_key)
+    .then(function (response) {
+      console.log(response);
+      commit("add", {
+        id: 1
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    //   setTimeout(() => {
+    //     commit("add", {
+    //         id: 1
+    //       })
+    //   }, 1000);
   }
